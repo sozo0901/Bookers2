944f315d5d678346896bdfc8dd85770b0f1debe3
taiki.ishikawa.97@gmail.com <ec2-user@ip-172-31-1-62.ec2.internal>
2021-09-16 08:34:18 +0000

    [Finish] RSpec実行済み

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,10 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+
+    if @book.user_id != current_user.id
+      redirect_to books_path
+    end
   end
 
   def update
